af6155a0e1da6e5f8653b3716a45aa87d99578be
Iosif Pravets <i@pravets.ru>
2025-04-24 22:19:44 +0300

    Изменен keymap.c
    - добавлен слой функциональных клавиш
    - изменены клавиши на нулевом слое

--- keyboards/crazy_keyboards/cygnus_rp2040/keymaps/default/keymap.c
+++ keyboards/crazy_keyboards/cygnus_rp2040/keymaps/default/keymap.c
@@ -22,6 +22,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_ENT,  MO(1),   KC_RALT
+    ),
+    [1] = LAYOUT_split_3x5_3(
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_GRAVE,                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_NO,                              KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LBRC,
+        KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,                              KC_NO,   KC_RBRC, KC_NO,   KC_NO,   KC_NO,
+                                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
     )
+
 };
