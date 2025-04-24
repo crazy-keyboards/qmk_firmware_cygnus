e0c43649cb976bc8b97bbe2ee227875d6ffedc12
Iosif Pravets <i@pravets.ru>
2025-04-24 22:22:01 +0300

    Добавлен config.h
    - добавлены настройки для контроллера

---
+++ keyboards/crazy_keyboards/cygnus_rp2040/config.h
@@ -0,0 +1,10 @@
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define COMBO_ONLY_FROM_LAYER 0
+
+/* RP2040- and hardware-specific config */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
