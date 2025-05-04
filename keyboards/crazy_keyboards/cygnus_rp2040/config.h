fff42d6cfe714ca26b9f4fe88f72d9b127e504b2
Iosif Pravets <i@pravets.ru>
2025-05-04 19:29:37 +0300

    раскладка wellum для cygnus_rp2040

--- keyboards/crazy_keyboards/cygnus_rp2040/config.h
+++ keyboards/crazy_keyboards/cygnus_rp2040/config.h
@@ -3,8 +3,12 @@
 
 #pragma once
 
-#define COMBO_ONLY_FROM_LAYER 0
+// #define COMBO_ONLY_FROM_LAYER 0
 
 /* RP2040- and hardware-specific config */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+// default but used in macros
+// #undef TAPPING_TERM
+// #define TAPPING_TERM 200
